75520844ffd1f965e687a5c62e4919962adfa007
John Ayres <john@metabroadcast.com>
2010-08-09 10:53:14 +0100

    convert locations to ondemands

--- src/main/java/org/atlasapi/feeds/interlinking/PlaylistToInterlinkFeedAdapter.java
+++ src/main/java/org/atlasapi/feeds/interlinking/PlaylistToInterlinkFeedAdapter.java
@@ -1,14 +1,21 @@
 package org.atlasapi.feeds.interlinking;
 
+import java.util.Set;
+
 import org.atlasapi.feeds.interlinking.InterlinkFeed.InterlinkFeedAuthor;
+import org.atlasapi.media.TransportType;
 import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Broadcast;
+import org.atlasapi.media.entity.Encoding;
 import org.atlasapi.media.entity.Episode;
 import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.Location;
 import org.atlasapi.media.entity.Playlist;
 import org.atlasapi.media.entity.Version;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
+
+import com.google.common.collect.Sets;
 
 public class PlaylistToInterlinkFeedAdapter {
 
@@ -40,12 +47,27 @@ public class PlaylistToInterlinkFeedAdapter {
 		InterlinkEpisode episode = new InterlinkEpisode(item.getCanonicalUri(), itemIndexFrom(item))
 			.withTitle(item.getTitle());
 		
-		for (Version version : item.getVersions()) {
-			for (Broadcast broadcast : version.getBroadcasts()) {
-				episode.addBroadcast(fromBroadcast(broadcast));
-			}
+		for (Broadcast broadcast : broadcasts(item)) {
+			episode.addBroadcast(fromBroadcast(broadcast));
 		}
+		
+		Location linkLocation = firstLinkLocation(item);
+
+		if (linkLocation != null) {
+			episode.addOnDemand(fromLocation(linkLocation));
+		}
+		
 		return episode;
+	}
+
+	private InterlinkBrand fromBrand(Brand brand) {
+		InterlinkBrand linkBrand = new InterlinkBrand(brand.getCanonicalUri());
+		linkBrand.withTitle(brand.getTitle());
+		return linkBrand;
+	}
+	
+	private InterlinkOnDemand fromLocation(Location linkLocation) {
+		return new InterlinkOnDemand(linkLocation.getUri());
 	}
 
 	private InterlinkBroadcast fromBroadcast(Broadcast broadcast) {
@@ -55,26 +77,42 @@ public class PlaylistToInterlinkFeedAdapter {
 			.withDuration(toDuration(broadcast.getBroadcastDuration()))
 			.withBroadcastStart(broadcast.getTransmissionTime());
 		return linkBroadcast;
-		
+	}
+	
+	private static Set<Broadcast> broadcasts(Item item) {
+		Set<Broadcast> broadcasts = Sets.newHashSet();
+		for (Version version : item.getVersions()) {
+			for (Broadcast broadcast : version.getBroadcasts()) {
+				broadcasts.add(broadcast);
+			}
+		}
+		return broadcasts;
 	}
 
-	private Duration toDuration(Integer seconds) {
-		if (seconds != null) {
-			return Duration.standardSeconds(seconds);
+	private static Location firstLinkLocation(Item item) {
+		for (Version version : item.getVersions()) {
+			for (Encoding encoding : version.getManifestedAs()) {
+				for (Location location : encoding.getAvailableAt()) {
+					if (TransportType.LINK.equals(location.getTransportType())) {
+						return location;
+					}
+				}
+			}
 		}
 		return null;
 	}
 
-	private Integer itemIndexFrom(Item item) {
+	private static Integer itemIndexFrom(Item item) {
 		if (item instanceof Episode) {
 			return ((Episode) item).getEpisodeNumber();
 		}
 		return null;
 	}
 
-	private InterlinkBrand fromBrand(Brand brand) {
-		InterlinkBrand linkBrand = new InterlinkBrand(brand.getCanonicalUri());
-		linkBrand.withTitle(brand.getTitle());
-		return linkBrand;
+	private static Duration toDuration(Integer seconds) {
+		if (seconds != null) {
+			return Duration.standardSeconds(seconds);
+		}
+		return null;
 	}
 }
