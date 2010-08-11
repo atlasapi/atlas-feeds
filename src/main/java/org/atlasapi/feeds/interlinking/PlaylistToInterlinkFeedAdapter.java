d89d4a79e4d75bdeb08263cada97a6b7c0d53582
Ben Smith <ben.thesmith@gmail.com>
2010-08-11 16:56:57 +0100

    broadcasts must have correct broadcast atom-format id

--- src/main/java/org/atlasapi/feeds/interlinking/PlaylistToInterlinkFeedAdapter.java
+++ src/main/java/org/atlasapi/feeds/interlinking/PlaylistToInterlinkFeedAdapter.java
@@ -21,121 +21,126 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 public class PlaylistToInterlinkFeedAdapter {
+    
+    private static final String BROADCAST_ID_PREFIX = "tag:";
 
-	public InterlinkFeed fromPlaylist(Playlist playlist) {
-		InterlinkFeed feed = new InterlinkFeed(playlist.getCanonicalUri());
-		
-		// TODO These are dummy entries
-		feed.withAuthor(new InterlinkFeedAuthor(playlist.getPublisher().name(), playlist.getPublisher().name()));
-		feed.withUpdatedAt(new DateTime());
-		
-		for (Playlist subPlaylist : playlist.getPlaylists()) {
-			if (subPlaylist instanceof Brand) {
-				InterlinkBrand brand = fromBrand((Brand) subPlaylist);
-				feed.addBrand(brand);
-				
-				Map<String, InterlinkSeries> seriesLookup = Maps.newHashMap();
+    public InterlinkFeed fromPlaylist(Playlist playlist) {
+        InterlinkFeed feed = new InterlinkFeed(playlist.getCanonicalUri());
 
-				for (Item item : subPlaylist.getItems()) {
-					
-					if (item instanceof Episode) {
-						Episode episode = (Episode) item;
-						Series series = episode.getSeriesSummary();
-						if (series != null) {
-							InterlinkSeries linkSeries = seriesLookup.get(series.getCanonicalUri());
-							if (linkSeries == null) {
-								linkSeries = fromSeries(series);
-								brand.addSeries(linkSeries);
-								seriesLookup.put(series.getCanonicalUri(), linkSeries);
-							}
-							linkSeries.addEpisode(fromItem(episode));
-							continue;
-						}
-					}
-					brand.addEpisodeWithoutASeries(fromItem(item));
-				}
-			}
-		}
-		return feed;
-	}
+        // TODO These are dummy entries
+        feed.withAuthor(new InterlinkFeedAuthor(playlist.getPublisher().name(), playlist.getPublisher().name()));
+        feed.withUpdatedAt(new DateTime());
 
-	private InterlinkSeries fromSeries(Series series) {
-		return new InterlinkSeries(series.getCanonicalUri(), series.getSeriesNumber())
-			.withTitle(series.getTitle())
-			.withSummary(series.getDescription());
+        for (Playlist subPlaylist : playlist.getPlaylists()) {
+            if (subPlaylist instanceof Brand) {
+                InterlinkBrand brand = fromBrand((Brand) subPlaylist);
+                feed.addBrand(brand);
 
-	}
+                Map<String, InterlinkSeries> seriesLookup = Maps.newHashMap();
 
-	private InterlinkEpisode fromItem(Item item) {
-		InterlinkEpisode episode = new InterlinkEpisode(item.getCanonicalUri(), itemIndexFrom(item))
-			.withTitle(item.getTitle());
-		
-		for (Broadcast broadcast : broadcasts(item)) {
-			episode.addBroadcast(fromBroadcast(broadcast));
-		}
-		
-		Location linkLocation = firstLinkLocation(item);
+                for (Item item : subPlaylist.getItems()) {
 
-		if (linkLocation != null) {
-			episode.addOnDemand(fromLocation(linkLocation));
-		}
-		
-		return episode;
-	}
+                    if (item instanceof Episode) {
+                        Episode episode = (Episode) item;
+                        Series series = episode.getSeriesSummary();
+                        if (series != null) {
+                            InterlinkSeries linkSeries = seriesLookup.get(series.getCanonicalUri());
+                            if (linkSeries == null) {
+                                linkSeries = fromSeries(series);
+                                brand.addSeries(linkSeries);
+                                seriesLookup.put(series.getCanonicalUri(), linkSeries);
+                            }
+                            linkSeries.addEpisode(fromItem(episode));
+                            continue;
+                        }
+                    }
+                    brand.addEpisodeWithoutASeries(fromItem(item));
+                }
+            }
+        }
+        return feed;
+    }
 
-	private InterlinkBrand fromBrand(Brand brand) {
-		return new InterlinkBrand(brand.getCanonicalUri())
-			.withTitle(brand.getTitle())
-			.withSummary(brand.getDescription());
-	}
-	
-	private InterlinkOnDemand fromLocation(Location linkLocation) {
-		return new InterlinkOnDemand(linkLocation.getUri());
-	}
+    private InterlinkSeries fromSeries(Series series) {
+        return new InterlinkSeries(series.getCanonicalUri(), series.getSeriesNumber()).withTitle(series.getTitle()).withSummary(series.getDescription());
+    }
 
-	private InterlinkBroadcast fromBroadcast(Broadcast broadcast) {
-		// use a generated id for now until ids are in the model
-		String generatedId = broadcast.getBroadcastOn() + "-" + broadcast.getTransmissionTime().getMillis();
-		InterlinkBroadcast linkBroadcast = new InterlinkBroadcast(generatedId)
-			.withDuration(toDuration(broadcast.getBroadcastDuration()))
-			.withBroadcastStart(broadcast.getTransmissionTime());
-		return linkBroadcast;
-	}
-	
-	private static Set<Broadcast> broadcasts(Item item) {
-		Set<Broadcast> broadcasts = Sets.newHashSet();
-		for (Version version : item.getVersions()) {
-			for (Broadcast broadcast : version.getBroadcasts()) {
-				broadcasts.add(broadcast);
-			}
-		}
-		return broadcasts;
-	}
+    private InterlinkEpisode fromItem(Item item) {
+        InterlinkEpisode episode = new InterlinkEpisode(item.getCanonicalUri(), itemIndexFrom(item)).withTitle(item.getTitle());
 
-	private static Location firstLinkLocation(Item item) {
-		for (Version version : item.getVersions()) {
-			for (Encoding encoding : version.getManifestedAs()) {
-				for (Location location : encoding.getAvailableAt()) {
-					if (TransportType.LINK.equals(location.getTransportType())) {
-						return location;
-					}
-				}
-			}
-		}
-		return null;
-	}
+        for (Broadcast broadcast : broadcasts(item)) {
+            InterlinkBroadcast interlinkBroadcast = fromBroadcast(broadcast);
+            if (interlinkBroadcast != null) {
+                episode.addBroadcast(interlinkBroadcast);
+            }
+        }
 
-	private static Integer itemIndexFrom(Item item) {
-		if (item instanceof Episode) {
-			return ((Episode) item).getEpisodeNumber();
-		}
-		return null;
-	}
+        Location linkLocation = firstLinkLocation(item);
 
-	private static Duration toDuration(Integer seconds) {
-		if (seconds != null) {
-			return Duration.standardSeconds(seconds);
-		}
-		return null;
-	}
+        if (linkLocation != null) {
+            episode.addOnDemand(fromLocation(linkLocation));
+        }
+
+        return episode;
+    }
+
+    private InterlinkBrand fromBrand(Brand brand) {
+        return new InterlinkBrand(brand.getCanonicalUri()).withTitle(brand.getTitle()).withSummary(brand.getDescription());
+    }
+
+    private InterlinkOnDemand fromLocation(Location linkLocation) {
+        return new InterlinkOnDemand(linkLocation.getUri());
+    }
+
+    private InterlinkBroadcast fromBroadcast(Broadcast broadcast) {
+        String id = null;
+        for (String alias : broadcast.getAliases()) {
+            if (alias.startsWith(BROADCAST_ID_PREFIX) || alias.startsWith("urn:"+BROADCAST_ID_PREFIX)) {
+                id = alias;
+                break;
+            }
+        }
+
+        if (id != null) {
+            return new InterlinkBroadcast(id).withDuration(toDuration(broadcast.getBroadcastDuration())).withBroadcastStart(broadcast.getTransmissionTime());
+        }
+        return null;
+    }
+
+    private static Set<Broadcast> broadcasts(Item item) {
+        Set<Broadcast> broadcasts = Sets.newHashSet();
+        for (Version version : item.getVersions()) {
+            for (Broadcast broadcast : version.getBroadcasts()) {
+                broadcasts.add(broadcast);
+            }
+        }
+        return broadcasts;
+    }
+
+    private static Location firstLinkLocation(Item item) {
+        for (Version version : item.getVersions()) {
+            for (Encoding encoding : version.getManifestedAs()) {
+                for (Location location : encoding.getAvailableAt()) {
+                    if (TransportType.LINK.equals(location.getTransportType())) {
+                        return location;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static Integer itemIndexFrom(Item item) {
+        if (item instanceof Episode) {
+            return ((Episode) item).getEpisodeNumber();
+        }
+        return null;
+    }
+
+    private static Duration toDuration(Integer seconds) {
+        if (seconds != null) {
+            return Duration.standardSeconds(seconds);
+        }
+        return null;
+    }
 }
