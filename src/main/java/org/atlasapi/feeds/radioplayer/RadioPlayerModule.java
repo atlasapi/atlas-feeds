650af2cef24d3ef90bc1abcf8d0919ad28d70ca4
Fred van den Driessche <fred@metabroadcast.com>
2010-12-23 10:59:51 +0000

    added radioplayer scheduled task logging

--- src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerModule.java
+++ src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerModule.java
@@ -5,6 +5,8 @@ import javax.annotation.PostConstruct;
 import org.atlasapi.feeds.radioplayer.upload.RadioPlayerFileUploader;
 import org.atlasapi.persistence.content.query.KnownTypeQueryExecutor;
 import org.atlasapi.persistence.logging.AdapterLog;
+import org.atlasapi.persistence.logging.AdapterLogEntry;
+import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
 import org.joda.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,7 +25,7 @@ public class RadioPlayerModule {
 
 	private @Autowired @Qualifier("mongoDbQueryExcutorThatFiltersUriQueries") KnownTypeQueryExecutor queryExecutor;
 	
-	private @Value("${rp.ftp.enabled}") Boolean upload;
+	private @Value("${rp.ftp.enabled}") String upload;
     private @Value("${rp.ftp.username}") String ftpUsername;
     private @Value("${rp.ftp.password}") String ftpPassword;
     private @Value("${rp.ftp.host}") String ftpHost;
@@ -39,8 +41,14 @@ public class RadioPlayerModule {
 	
 	@PostConstruct 
 	public void scheduleTasks() {
-		if(upload) {
+		if (Boolean.parseBoolean(upload)) {
 			scheduler.schedule(new RadioPlayerFileUploader(ftpHost, ftpPort, ftpUsername, ftpPassword, ftpPath, queryExecutor, log), UPLOAD);
+			log.record(new AdapterLogEntry(Severity.INFO)
+            .withDescription("Radioplayer uploader scheduled task installed")
+            .withSource(RadioPlayerFileUploader.class));
+		} else {
+			log.record(new AdapterLogEntry(Severity.INFO)
+			.withDescription("Not installing Radioplayer uploader"));
 		}
 	}
 	
