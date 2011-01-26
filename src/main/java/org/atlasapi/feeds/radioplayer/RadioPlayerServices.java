f033eaa8b4412d46fe2772c13914dcf262b706a2
Fred van den Driessche <fred@metabroadcast.com>
2011-01-26 18:37:46 +0000

    complete ion service ids

--- src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerServices.java
+++ src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerServices.java
@@ -18,16 +18,16 @@ public class RadioPlayerServices {
 			add(new RadioPlayerService(300, "london").withIonServiceId("bbc_london")).
 			add(new RadioPlayerService(301, "berkshire")).
 			add(new RadioPlayerService(302, "bristol")).
-			add(new RadioPlayerService(303, "cambridgeshire")).
+			add(new RadioPlayerService(303, "cambridgeshire").withIonServiceId("bbc_radio_cambridge")).
 			add(new RadioPlayerService(304, "cornwall")).
-			add(new RadioPlayerService(305, "coventry")).
+			add(new RadioPlayerService(305, "coventry").withIonServiceId("bbc_radio_coventry_warwickshire")).
 			add(new RadioPlayerService(306, "cumbria")).
 			add(new RadioPlayerService(307, "derby")).
 			add(new RadioPlayerService(308, "devon")).
 			add(new RadioPlayerService(309, "essex")).
 			add(new RadioPlayerService(310, "gloucestershire")).
 			add(new RadioPlayerService(311, "guernsey")).
-			add(new RadioPlayerService(312, "herefordandworcester")).
+			add(new RadioPlayerService(312, "herefordandworcester").withIonServiceId("bbc_radio_hereford_worcester")).
 			add(new RadioPlayerService(313, "humberside")).
 			add(new RadioPlayerService(314, "jersey")).
 			add(new RadioPlayerService(315, "kent")).
@@ -45,7 +45,7 @@ public class RadioPlayerServices {
 			add(new RadioPlayerService(327, "sheffield")).
 			add(new RadioPlayerService(328, "shropshire")).
 			add(new RadioPlayerService(329, "solent")).
-			add(new RadioPlayerService(330, "somerset")).
+			add(new RadioPlayerService(330, "somerset").withIonServiceId("bbc_radio_somerset_sound")).
 			add(new RadioPlayerService(331, "stoke")).
 			add(new RadioPlayerService(332, "suffolk")).
 			add(new RadioPlayerService(333, "surrey")).
@@ -53,21 +53,21 @@ public class RadioPlayerServices {
 			add(new RadioPlayerService(335, "wiltshire")).
 			add(new RadioPlayerService(336, "york")).
 			add(new RadioPlayerService(337, "tees").withIonServiceId("bbc_tees")).
-			add(new RadioPlayerService(338, "threecounties")).
+			add(new RadioPlayerService(338, "threecounties").withIonServiceId("bbc_three_counties_radio")).
 			add(new RadioPlayerService(339, "wm").withIonServiceId("bbc_wm")).
 			add(new RadioPlayerService(340, "radio1").withServiceUriSuffix("radio1/england").withScheduleUri("http://www.bbc.co.uk/radio1/programmes/schedules/england").withIonServiceId("bbc_radio_one")).
 			add(new RadioPlayerService(341, "1xtra").withIonServiceId("bbc_1xtra")).
 			add(new RadioPlayerService(342, "radio2").withIonServiceId("bbc_radio_two")).
 			add(new RadioPlayerService(343, "radio3").withIonServiceId("bbc_radio_three")).
 			add(new RadioPlayerService(344, "radio4").withServiceUriSuffix("radio4/fm").withScheduleUri("http://www.bbc.co.uk/radio4/programmes/schedules/fm").withIonServiceId("bbc_radio_fourfm")).
-			add(new RadioPlayerService(345, "5live")).
-			add(new RadioPlayerService(346, "5livesportsextra")).
+			add(new RadioPlayerService(345, "5live").withIonServiceId("bbc_radio_five_live")).
+			add(new RadioPlayerService(346, "5livesportsextra").withIonServiceId("bbc_radio_five_live_sports_extra")).
 			add(new RadioPlayerService(347, "6music").withIonServiceId("bbc_6music")).
-			add(new RadioPlayerService(348, "radio7")).
+			add(new RadioPlayerService(348, "radio7").withIonServiceId("bbc_7")).
 			add(new RadioPlayerService(349, "asiannetwork").withIonServiceId("bbc_asian_network")).
 			add(new RadioPlayerService(350, "worldservice").withIonServiceId("bbc_world_service")).
 			add(new RadioPlayerService(351, "radioscotland").withServiceUriSuffix("radioscotland/fm").withScheduleUri("http://www.bbc.co.uk/radioscotland/programmes/schedules/fm").withIonServiceId("bbc_radio_scotland")).
-			add(new RadioPlayerService(352, "radionangaidheal").withIonServiceId("bbc_radio_nangaidheal")).
+			add(new RadioPlayerService(352, "radionangaidheal").withIonServiceId("bbc_radio_nangaidheal").withIonServiceId("bbc_radio_nan_gaidheal")).
 			add(new RadioPlayerService(353, "radioulster").withIonServiceId("bbc_radio_ulster")).
 			add(new RadioPlayerService(354, "radiofoyle").withIonServiceId("bbc_radio_foyle")).
 			add(new RadioPlayerService(355, "radiowales").withServiceUriSuffix("radiowales/fm").withScheduleUri("http://www.bbc.co.uk/radiowales/programmes/schedules/fm").withIonServiceId("bbc_radio_wales")).
