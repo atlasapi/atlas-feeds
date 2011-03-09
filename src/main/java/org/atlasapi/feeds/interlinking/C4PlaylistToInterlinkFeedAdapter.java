b121fbf6e5f61172f8e7b80c264da4d01258971b
Fred van den Driessche <fred@metabroadcast.com>
2011-03-09 12:22:56 +0000

    channel specific broadcast ids on output

--- src/main/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapter.java
+++ src/main/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapter.java
@@ -23,29 +23,39 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.MapMaker;
 
 public class C4PlaylistToInterlinkFeedAdapter extends PlaylistToInterlinkFeedAdapter {
-    
+
     private static final Pattern BROADCAST_ID_PATTERN = Pattern.compile("(?:urn:)?(tag:www\\.\\w+4\\.com.*)");
+
+    private final static Pattern CHANNEL_SPECIFIC_ID_PATTERN = Pattern.compile("tag:([^,]+),(\\d{4}):slot/(C4|M4|F4|E4|4M)(\\d+)");
+
     private static Set<String> BROADCAST_SERVICES = ImmutableSet.of(CHANNEL_FOUR.uri(), E_FOUR.uri(), MORE_FOUR.uri(), FOUR_MUSIC.uri(), FILM_4.uri());
-    
+
     private final ConcurrentMap<String, Series> seriesLookup;
 
     protected static final Pattern SYNTHESIZED_PATTERN = Pattern.compile("http://www.channel4.com/programmes/synthesized/[^/]+/(\\d+)");
-    
+
     public C4PlaylistToInterlinkFeedAdapter(final ContentResolver resolver) {
-    	seriesLookup = new MapMaker().expireAfterAccess(10, TimeUnit.MINUTES).makeComputingMap(new Function<String, Series>() {
-			@Override
-			public Series apply(String uri) {
-				return (Series) resolver.findByCanonicalUri(uri);
-			}
-    	});
+        seriesLookup = new MapMaker().expireAfterAccess(10, TimeUnit.MINUTES).makeComputingMap(new Function<String, Series>() {
+            @Override
+            public Series apply(String uri) {
+                return (Series) resolver.findByCanonicalUri(uri);
+            }
+        });
     }
-    
+
     @Override
     protected String broadcastId(Broadcast broadcast) {
     	for (String alias : broadcast.getAliases()) {
-			Matcher matcher = BROADCAST_ID_PATTERN.matcher(alias);
+    	    Matcher matcher = CHANNEL_SPECIFIC_ID_PATTERN.matcher(alias);
 			if (matcher.matches()) {
-				return matcher.group(1);
+				return alias;
+			}
+			//check if non-channel-specific alias
+			matcher = BROADCAST_ID_PATTERN.matcher(alias);
+			if (matcher.matches()) {
+			    String tagAlias = matcher.group(1);
+			    int slashIndex = tagAlias.lastIndexOf("/")+1;
+				return tagAlias.substring(0,slashIndex) + CHANNEL_LOOKUP.get(broadcast.getBroadcastOn()) + tagAlias.substring(slashIndex);
 			}
 		}
     	if(broadcast.getId() != null) {
@@ -53,41 +63,41 @@ public class C4PlaylistToInterlinkFeedAdapter extends PlaylistToInterlinkFeedAda
     	}
     	return super.broadcastId(broadcast);
     }
-    
+
     @Override
     protected Predicate<Broadcast> broadcastFilter() {
-    	return new Predicate<Broadcast>() {
+        return new Predicate<Broadcast>() {
 
-			@Override
-			public boolean apply(Broadcast broadcast) {
-				for (String alias : broadcast.getAliases()) {
-		            if (BROADCAST_ID_PATTERN.matcher(alias).matches()) {
-		            	return true;
-		            }
-		        }
-				return BROADCAST_SERVICES.contains(broadcast.getBroadcastOn());
-			}
-		};
+            @Override
+            public boolean apply(Broadcast broadcast) {
+                for (String alias : broadcast.getAliases()) {
+                    if (BROADCAST_ID_PATTERN.matcher(alias).matches()) {
+                        return true;
+                    }
+                }
+                return BROADCAST_SERVICES.contains(broadcast.getBroadcastOn());
+            }
+        };
     }
-    
+
     @Override
     protected String idFrom(Identified description) {
-    	// Lookup full series if it is an embedded series
-    	if (description instanceof Series) {
-    	    description = seriesLookup.get(description.getCanonicalUri());
-    	}
-    	for (String alias : description.getAliases()) {
-			if (alias.startsWith("tag:www.channel4.com") || alias.startsWith("urn:tag:www.channel4.com") || alias.startsWith("tag:www.e4.com")) {
-				return alias;
-			}
-		}
-    	return description.getCanonicalUri();
+        // Lookup full series if it is an embedded series
+        if (description instanceof Series) {
+            description = seriesLookup.get(description.getCanonicalUri());
+        }
+        for (String alias : description.getAliases()) {
+            if (alias.startsWith("tag:www.channel4.com") || alias.startsWith("urn:tag:www.channel4.com") || alias.startsWith("tag:www.e4.com")) {
+                return alias;
+            }
+        }
+        return description.getCanonicalUri();
     }
-    
+
     @Override
     protected String linkFrom(String canonicalUri) {
         Matcher matcher = SYNTHESIZED_PATTERN.matcher(canonicalUri);
-        if(matcher.matches()) {
+        if (matcher.matches()) {
             return "http://www.channel4.com/tv-listings#" + matcher.group(1);
         }
         return canonicalUri;
---
+++ src/test/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapterTest.java
@@ -0,0 +1,32 @@
+package org.atlasapi.feeds.interlinking;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.atlasapi.media.entity.Broadcast;
+import org.atlasapi.media.entity.Channel;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import com.metabroadcast.common.time.DateTimeZones;
+
+public class C4PlaylistToInterlinkFeedAdapterTest {
+
+    @Test
+    public void testBroadcastId() {
+        
+        C4PlaylistToInterlinkFeedAdapter adapter = new C4PlaylistToInterlinkFeedAdapter(null);
+        
+        Broadcast b1 = new Broadcast(Channel.E_FOUR.uri(), new DateTime(DateTimeZones.UTC), new DateTime(DateTimeZones.UTC)).withId("Not used");
+        b1.addAlias("tag:www.e4.com,2009:slot/10401");
+        
+        assertThat(adapter.broadcastId(b1), is("tag:www.e4.com,2009:slot/E410401"));
+
+        Broadcast b2 = new Broadcast(Channel.E_FOUR.uri(), new DateTime(DateTimeZones.UTC), new DateTime(DateTimeZones.UTC)).withId("Not used");
+        b2.addAlias("tag:www.e4.com,2009:slot/E410401");
+        
+        assertThat(adapter.broadcastId(b2), is("tag:www.e4.com,2009:slot/E410401"));
+        
+    }
+
+}
