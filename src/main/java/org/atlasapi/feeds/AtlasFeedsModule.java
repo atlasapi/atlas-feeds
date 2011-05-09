4825f00e011eba71562a970bc8812b107c3391f2
John Ayres <john@metabroadcast.com>
2011-05-09 10:53:30 +0100

    run heavyweight uploader on the processing node to avoid locking the frontend

--- src/main/java/org/atlasapi/feeds/AtlasFeedsModule.java
+++ src/main/java/org/atlasapi/feeds/AtlasFeedsModule.java
@@ -1,13 +1,12 @@
 package org.atlasapi.feeds;
 
 import org.atlasapi.feeds.interlinking.InterlinkingModule;
-import org.atlasapi.feeds.radioplayer.RadioPlayerModule;
 import org.atlasapi.feeds.sitemaps.SiteMapModule;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({InterlinkingModule.class, SiteMapModule.class, RadioPlayerModule.class})
+@Import({InterlinkingModule.class, SiteMapModule.class})
 public class AtlasFeedsModule {
 
 }
