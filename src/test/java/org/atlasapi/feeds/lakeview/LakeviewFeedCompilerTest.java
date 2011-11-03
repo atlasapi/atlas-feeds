17201e662b0d1937b5a5e83da8cf3fa991017c7a
Tom McAdam <tom@metabroadcast.com>
2011-11-03 15:10:54 +0000

    Dont send ApplicationSpecificData for TVSeries and TVSeason, and change format of URL for TVEpisode

--- src/test/java/org/atlasapi/feeds/lakeview/LakeviewFeedCompilerTest.java
+++ src/test/java/org/atlasapi/feeds/lakeview/LakeviewFeedCompilerTest.java
@@ -9,21 +9,21 @@ public class LakeviewFeedCompilerTest {
 	@Test
 	public void testBrandAtomUri() {
 		LakeviewFeedCompiler feedCompiler = new LakeviewFeedCompiler();
-		assertEquals("http://www.channel4.com/pmlsd/educating-essex/4od.atom", 
+		assertEquals("https://xbox.channel4.com/pmlsd/educating-essex/4od.atom", 
 				feedCompiler.brandAtomUri("http://www.channel4.com/programmes/educating-essex"));
 	}
 	
 	@Test
 	public void testSeriesAtomUri() {
 		LakeviewFeedCompiler feedCompiler = new LakeviewFeedCompiler();
-		assertEquals("http://www.channel4.com/pmlsd/educating-essex/4od.atom#series-1", 
+		assertEquals("https://xbox.channel4.com/pmlsd/educating-essex/4od.atom#series-1", 
 				feedCompiler.seriesAtomUri("http://www.channel4.com/programmes/educating-essex/episode-guide/series-1"));
 	}
 
 	@Test
 	public void testEpisodeAtomUri() {
 		LakeviewFeedCompiler feedCompiler = new LakeviewFeedCompiler();
-		assertEquals("http://www.channel4.com/pmlsd/educating-essex/4od.atom#12345", 
+		assertEquals("https://xbox.channel4.com/pmlsd/educating-essex/4od.atom#12345", 
 				feedCompiler.episodeAtomUri("http://www.channel4.com/programmes/educating-essex/episode-guide/series-1/episode-1", "12345"));
 	
 	}
