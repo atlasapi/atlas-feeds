16886ac548d6b787d313abe0135b4911d2f7d15c
Fred van den Driessche <fred@metabroadcast.com>
2011-12-02 10:12:17 +0000

    initial xmltv feed outputter and controller

--- src/main/java/org/atlasapi/feeds/AtlasFeedsModule.java
+++ src/main/java/org/atlasapi/feeds/AtlasFeedsModule.java
@@ -3,11 +3,12 @@ package org.atlasapi.feeds;
 import org.atlasapi.feeds.interlinking.InterlinkingModule;
 import org.atlasapi.feeds.lakeview.LakeviewModule;
 import org.atlasapi.feeds.sitemaps.SiteMapModule;
+import org.atlasapi.feeds.xmltv.XmlTvModule;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({InterlinkingModule.class, SiteMapModule.class, LakeviewModule.class})
+@Import({InterlinkingModule.class, SiteMapModule.class, LakeviewModule.class, XmlTvModule.class})
 public class AtlasFeedsModule {
 
 }
