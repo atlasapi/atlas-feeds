1d647aa000fa09e0a410f95701ceacfc42ef67e7
Oli Hall <oli@metabradcast.com>
2013-04-15 17:44:15 +0100

    Don't restrict type of brand/series children to Episode, genericise to Item

--- src/main/java/org/atlasapi/feeds/tvanytime/GroupInformationGenerator.java
+++ src/main/java/org/atlasapi/feeds/tvanytime/GroupInformationGenerator.java
@@ -3,6 +3,7 @@ package org.atlasapi.feeds.tvanytime;
 import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Episode;
 import org.atlasapi.media.entity.Film;
+import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Series;
 
 import tva.metadata._2010.GroupInformationType;
@@ -10,6 +11,6 @@ import tva.metadata._2010.GroupInformationType;
 public interface GroupInformationGenerator {
     public GroupInformationType generate(Film film);
     public GroupInformationType generate(Episode episode);
-    public GroupInformationType generate(Series series, Episode episode);
-    public GroupInformationType generate(Brand brand, Episode episode);
+    public GroupInformationType generate(Series series, Item item);
+    public GroupInformationType generate(Brand brand, Item item);
 }
