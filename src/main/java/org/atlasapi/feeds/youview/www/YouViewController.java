98b3342d1b5eb1b914497444b42b76ffb3c6d84a
Oli Hall <oli@metabradcast.com>
2013-05-29 10:38:50 +0100

    Add a debug endpoint to generate the youview lovefilm TVA xml for a single item

--- src/main/java/org/atlasapi/feeds/youview/www/YouViewController.java
+++ src/main/java/org/atlasapi/feeds/youview/www/YouViewController.java
@@ -1,6 +1,8 @@
 package org.atlasapi.feeds.youview.www;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +11,10 @@ import org.atlasapi.application.ApplicationConfiguration;
 import org.atlasapi.application.query.ApplicationConfigurationFetcher;
 import org.atlasapi.feeds.tvanytime.TvAnytimeGenerator;
 import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.persistence.content.mongo.LastUpdatedContentFinder;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
@@ -20,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.metabroadcast.common.base.Maybe;
 import com.metabroadcast.common.media.MimeType;
 import com.metabroadcast.common.time.DateTimeZones;
@@ -34,12 +40,14 @@ public class YouViewController {
     private final DateTimeFormatter fmt = ISODateTimeFormat.dateHourMinuteSecond().withZone(DateTimeZones.UTC);
     private final TvAnytimeGenerator feedGenerator;
     private final LastUpdatedContentFinder contentFinder;
+    private final ContentResolver contentResolver;
     private final ApplicationConfigurationFetcher configFetcher;
     
-    public YouViewController(ApplicationConfigurationFetcher configFetcher, TvAnytimeGenerator feedGenerator, LastUpdatedContentFinder contentFinder) {
+    public YouViewController(ApplicationConfigurationFetcher configFetcher, TvAnytimeGenerator feedGenerator, LastUpdatedContentFinder contentFinder, ContentResolver contentResolver) {
         this.configFetcher = configFetcher;
         this.feedGenerator = feedGenerator;
         this.contentFinder = contentFinder;
+        this.contentResolver = contentResolver;
     }
     
     /**
@@ -65,6 +73,29 @@ public class YouViewController {
             
             Optional<String> since = Optional.fromNullable(lastUpdated);
             feedGenerator.generateXml(getContentSinceDate(since), response.getOutputStream());
+            
+        } catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.setContentLength(0);
+        }
+    }
+    
+    @RequestMapping("/feeds/youview/lovefilm/content")
+    public void generateFeedForItem(HttpServletRequest request, HttpServletResponse response,
+            @RequestParam(value = "uri", required = true) String uri) throws IOException {
+        try {
+            final ApplicationConfiguration appConfig = appConfig(request);
+            if (!appConfig.isEnabled(PUBLISHER)) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.setContentLength(0);
+                return;
+            }
+            response.setContentType(MimeType.APPLICATION_ATOM_XML.toString());
+            response.setStatus(HttpServletResponse.SC_OK);
+            
+            ResolvedContent resolvedContent = contentResolver.findByCanonicalUris(ImmutableList.of(uri));
+            Collection<Identified> resolved = resolvedContent.asResolvedMap().values();
+            feedGenerator.generateXml(ImmutableList.of((Content) Iterables.getOnlyElement(resolved)), response.getOutputStream());
             
         } catch (IOException e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
