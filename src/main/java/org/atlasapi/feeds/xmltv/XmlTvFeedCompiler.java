b4451c539b42819a5ddb48ceb6ea7c2b5277e178
Tom McAdam <tom@metabroadcast.com>
2013-06-07 16:01:06 +0100

    LookupRefs now also have the ID

--- src/main/java/org/atlasapi/feeds/xmltv/XmlTvFeedCompiler.java
+++ src/main/java/org/atlasapi/feeds/xmltv/XmlTvFeedCompiler.java
@@ -103,7 +103,8 @@ public class XmlTvFeedCompiler {
             @Override
             public LookupRef apply(Episode input) {
                 if(input.getSeriesRef() != null) {
-                    return new LookupRef(input.getSeriesRef().getUri(), input.getPublisher(), ContentCategory.PROGRAMME_GROUP);
+                    return new LookupRef(input.getSeriesRef().getUri(), input.getSeriesRef().getId(), 
+                            input.getPublisher(), ContentCategory.PROGRAMME_GROUP);
                 }
                 return null;
             }
@@ -123,7 +124,8 @@ public class XmlTvFeedCompiler {
             @Override
             public LookupRef apply(Item input) {
                 if(input.getContainer() != null) {
-                    return new LookupRef(input.getContainer().getUri(), input.getPublisher(), ContentCategory.CONTAINER);
+                    return new LookupRef(input.getContainer().getUri(), input.getContainer().getId(),
+                            input.getPublisher(), ContentCategory.CONTAINER);
                 }
                 return null;
             }
