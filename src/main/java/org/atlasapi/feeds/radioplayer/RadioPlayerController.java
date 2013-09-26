3ff3e21828dcd14d97059c394a313991d276703c
Oli Hall <oli@metabradcast.com>
2013-09-26 11:08:10 +0100

    Selection of fixes.  - Ensure clip outputter uses canonical uri rather than curie.  - file controller takes publisher, so files are generated per publisher rather than just for a single publisher

--- src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerController.java
+++ src/main/java/org/atlasapi/feeds/radioplayer/RadioPlayerController.java
@@ -4,6 +4,8 @@ import static org.atlasapi.feeds.radioplayer.upload.FileType.OD;
 import static org.atlasapi.feeds.radioplayer.upload.FileType.PI;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,21 +19,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.collect.Maps;
 
 @Controller
 public class RadioPlayerController {
     
-    private final RadioPlayerOdUriResolver odUriResolver;
-    private final Publisher publisher;
+    private static final Joiner JOIN_ON_COMMA = Joiner.on(',');
+    
+    private final Map<Publisher, RadioPlayerOdUriResolver> odUriResolver;
+    private final Set<Publisher> publishers;
 
-    public RadioPlayerController(LastUpdatedContentFinder lastUpdatedContentFinder, ContentLister contentLister, Publisher publisher) {
-        this.publisher = publisher;
-        odUriResolver = new RadioPlayerOdUriResolver(contentLister, lastUpdatedContentFinder, publisher);
+    public RadioPlayerController(final LastUpdatedContentFinder lastUpdatedContentFinder, final ContentLister contentLister, Set<Publisher> publishers) {
+        this.publishers = publishers;
+        this.odUriResolver = Maps.asMap(publishers, new Function<Publisher, RadioPlayerOdUriResolver> () {
+            @Override
+            public RadioPlayerOdUriResolver apply(Publisher input) {
+                return new RadioPlayerOdUriResolver(contentLister, lastUpdatedContentFinder, input);
+            }
+        });
     }
-
-    @RequestMapping("feeds/ukradioplayer/{filename}.xml")
-    public void xmlForFilename(@PathVariable("filename") String filename, HttpServletResponse response) throws IOException {
+    
+    @RequestMapping("feeds/{publisher}/ukradioplayer/{filename}.xml")
+    public void xmlForFilename(@PathVariable("publisher") String publisherStr, @PathVariable("filename") String filename, 
+            HttpServletResponse response) throws IOException {
 
         RadioPlayerFilenameMatcher matcher = RadioPlayerFilenameMatcher.on(filename);
 
@@ -39,17 +52,25 @@ public class RadioPlayerController {
 
             FileType feedType = matcher.type().requireValue();
             try {
+                Publisher publisher = Publisher.valueOf(publisherStr.trim().toUpperCase());
+                RadioPlayerOdUriResolver publisherOdUriResolver = odUriResolver.get(publisher);
+                if (publisherOdUriResolver == null) {
+                    response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Publisher not allowed. Must be one of " + JOIN_ON_COMMA.join(publishers));
+                }
+                 
                 RadioPlayerFeedSpec spec;
                 if (matcher.type().requireValue().equals(PI)) {
                     spec = new RadioPlayerPiFeedSpec(matcher.service().requireValue(), matcher.date().requireValue());
                 } else if (matcher.type().requireValue().equals(OD)) {
                     DateTime since = matcher.date().requireValue().toDateTimeAtStartOfDay().minusHours(2);
-                    spec = new RadioPlayerOdFeedSpec(matcher.service().requireValue(), matcher.date().requireValue(), Optional.of(since), odUriResolver.getServiceToUrisMapSince(since).get(matcher.service().requireValue()));
+                    spec = new RadioPlayerOdFeedSpec(matcher.service().requireValue(), matcher.date().requireValue(), Optional.of(since), publisherOdUriResolver.getServiceToUrisMapSince(since).get(matcher.service().requireValue()));
                 } else {
                     throw new IllegalArgumentException("Unknown file type");
                 }
                 
                 RadioPlayerFeedCompiler.valueOf(publisher, feedType).compileFeedFor(spec, response.getOutputStream());
+            } catch (IllegalArgumentException e) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid publisher" + e.getMessage());
             } catch (Exception e) {
                 response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
             }
