de6f89178c004e7e3ba26323561cc77b886f74d6
Oli Hall <oli@metabradcast.com>
2013-09-26 15:03:50 +0100

    Ensure nitro urls are coped with when creating the audio source urls

--- src/main/java/org/atlasapi/feeds/radioplayer/outputting/RadioPlayerProgrammeInformationOutputter.java
+++ src/main/java/org/atlasapi/feeds/radioplayer/outputting/RadioPlayerProgrammeInformationOutputter.java
@@ -288,7 +288,7 @@ public class RadioPlayerProgrammeInformationOutputter extends RadioPlayerXMLOutp
     private String audioSourceUrl(String versionUri) {
         return String.format(
             "http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/proto/http/format/json/vpid/%s/mediaset/",
-            versionUri.replace("http://www.bbc.co.uk/programmes/","")
+            versionUri.replaceAll("http://[a-z]*.bbc.co.uk/programmes/","")
         );
     }
 
