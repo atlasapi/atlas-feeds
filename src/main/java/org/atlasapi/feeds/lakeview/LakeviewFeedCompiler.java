5efdff570093539d2e431240d14ed627cc6df702
Tom McAdam <tom@metabroadcast.com>
2013-10-18 16:51:25 +0100

    Change ItemID for episodes to base from applicationspecificdata

--- src/main/java/org/atlasapi/feeds/lakeview/LakeviewFeedCompiler.java
+++ src/main/java/org/atlasapi/feeds/lakeview/LakeviewFeedCompiler.java
@@ -66,7 +66,8 @@ public class LakeviewFeedCompiler {
     private static final String C4_PROG_BASE = "http://www.channel4.com/programmes/";
     private static final String C4_API_BASE = "https://xbox.channel4.com/pmlsd/";
     
-    private static final String SERIES_ID_PREFIX = ID_PREFIX + "/TVSeries/"; 
+    private static final String SERIES_ID_PREFIX = ID_PREFIX + "/TVSeries/";
+    private static final String EPISODE_ID_PREFIX = ID_PREFIX + "/TVEpisode/";
     
     private final Clock clock;
 	private ChannelResolver channelResolver;
@@ -257,7 +258,7 @@ public class LakeviewFeedCompiler {
         } else {
             providerMediaId = brandId(container).replaceAll(SERIES_ID_PREFIX, "") + "#" + assetId;
         }
-        addIdElements(element, episodeId(episode), providerMediaId);
+        addIdElements(element, EPISODE_ID_PREFIX + providerMediaId, providerMediaId);
         
         if (genericTitlesEnabled) {
             if (episode.getEpisodeNumber() != null) {
