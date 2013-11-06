9916cbddf78ca2f51d77d5bef9017eab04096ed8
liamgh <liam@greenhughes.com>
2013-11-06 16:03:16 +0000

    Add comment about work for reorg

--- src/main/java/org/atlasapi/application/auth/InvalidApiKeyException.java
+++ src/main/java/org/atlasapi/application/auth/InvalidApiKeyException.java
@@ -1,6 +1,6 @@
 package org.atlasapi.application.auth;
 
-
+// TODO make this extend QueryExecutionException after reorg
 public class InvalidApiKeyException extends Exception {
     private static final long serialVersionUID = -8204400513571208163L;
     private final String apiKey;
