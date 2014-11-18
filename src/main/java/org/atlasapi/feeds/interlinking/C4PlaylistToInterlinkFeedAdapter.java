ce8da1c3ef8f380008aa304352b6f5a606182e77
Tom McAdam <tom@metabroadcast.com>
2014-11-18 15:38:11 +0000

    Create link URIs from aliases
    Conflicts:
    	src/main/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapter.java

--- src/main/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapter.java
+++ src/main/java/org/atlasapi/feeds/interlinking/C4PlaylistToInterlinkFeedAdapter.java
@@ -150,7 +150,7 @@ public class C4PlaylistToInterlinkFeedAdapter extends PlaylistToInterlinkFeedAda
         if (identified instanceof Episode) {
             return brandUriFromBrandCanonicalUri(((Episode)identified).getContainer().getUri());
         }
-        return WWW_CHANNEL4_PROGRAMMES_PREFIX;
+        throw new RuntimeException("Failed to extract link URI for " + identified);
     }
 
     private String brandUriFromBrandCanonicalUri(String uri) {
