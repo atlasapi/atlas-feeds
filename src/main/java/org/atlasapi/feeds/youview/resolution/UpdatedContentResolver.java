dffefeb961391f12ef77e8e1c23e751a36f33a36
Oli Hall <oli@metabroadcast.com>
2014-12-05 18:13:32 +0000

    Filter out audio content from the content returned by the content resolver.

--- src/main/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolver.java
+++ src/main/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolver.java
@@ -6,12 +6,15 @@ import static org.joda.time.DateTimeConstants.DECEMBER;
 import java.util.Iterator;
 
 import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.MediaType;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.mongo.LastUpdatedContentFinder;
 import org.joda.time.DateTime;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterators;
 import com.metabroadcast.common.time.DateTimeZones;
 
 
@@ -20,6 +23,13 @@ public class UpdatedContentResolver implements YouViewContentResolver {
     @VisibleForTesting
     // TODO update this to point back to the genuine start of time once testing is complete
     static final DateTime START_OF_TIME = new DateTime(2014, DECEMBER, 2, 0, 0, 0, 0, DateTimeZones.UTC);
+
+    private static final Predicate<Content> IS_VIDEO_CONTENT = new Predicate<Content>() {
+        @Override
+        public boolean apply(Content input) {
+            return MediaType.VIDEO.equals(input.getMediaType());
+        }
+    };
     
     private final LastUpdatedContentFinder contentFinder;
     private final Publisher publisher;
@@ -41,6 +51,6 @@ public class UpdatedContentResolver implements YouViewContentResolver {
     
     private Iterator<Content> fetchContentUpdatedSinceDate(Optional<DateTime> since) {
         DateTime start = since.isPresent() ? since.get() : START_OF_TIME;
-        return contentFinder.updatedSince(publisher, start);
+        return Iterators.filter(contentFinder.updatedSince(publisher, start), IS_VIDEO_CONTENT);
     }
 }
--- src/test/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolverTest.java
+++ src/test/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolverTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.atlasapi.media.entity.Content;
-import org.atlasapi.media.entity.Film;
+import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.MediaType;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.mongo.LastUpdatedContentFinder;
 import org.joda.time.DateTime;
@@ -21,8 +22,9 @@ public class UpdatedContentResolverTest {
     
     private static final Publisher PUBLISHER = Publisher.METABROADCAST;
     private static final DateTime RECENT_TIMESTAMP = DateTime.now();
-    private static final Content UPDATED_CONTENT = new Film("film", "curie", PUBLISHER);
-    private static final Content NOT_UPDATED_CONTENT = new Film("another-film", "curie", PUBLISHER);
+    private static final Content UPDATED_CONTENT = createItemWithMediaType(MediaType.VIDEO, "film");
+    private static final Content NOT_UPDATED_CONTENT = createItemWithMediaType(MediaType.VIDEO, "another-film");
+    private static final Content AUDIO_CONTENT = createItemWithMediaType(MediaType.AUDIO, "audio");
     
     private LastUpdatedContentFinder contentFinder = Mockito.mock(LastUpdatedContentFinder.class);
     
@@ -30,21 +32,28 @@ public class UpdatedContentResolverTest {
 
     @Before
     public void setup() {
-        when(contentFinder.updatedSince(PUBLISHER, RECENT_TIMESTAMP)).thenReturn(Iterators.forArray(UPDATED_CONTENT));
-        when(contentFinder.updatedSince(PUBLISHER, START_OF_TIME)).thenReturn(Iterators.forArray(NOT_UPDATED_CONTENT, UPDATED_CONTENT));
+        when(contentFinder.updatedSince(PUBLISHER, RECENT_TIMESTAMP)).thenReturn(Iterators.forArray(UPDATED_CONTENT, AUDIO_CONTENT));
+        when(contentFinder.updatedSince(PUBLISHER, START_OF_TIME)).thenReturn(Iterators.forArray(NOT_UPDATED_CONTENT, UPDATED_CONTENT, AUDIO_CONTENT));
     }
-    
+
     @Test
-    public void testFetchOfAllContentReturnsAllContent() {
+    public void testFetchOfAllContentReturnsAllVideoContent() {
         ImmutableSet<Content> content = ImmutableSet.copyOf(resolver.allContent());
         
         assertEquals(ImmutableSet.of(NOT_UPDATED_CONTENT, UPDATED_CONTENT), content);
     }
     
     @Test
-    public void testFetchOfContentUpdatedSinceDateReturnsOnlyUpdatedContent() {
+    public void testFetchOfContentUpdatedSinceDateReturnsOnlyUpdatedVideoContent() {
         ImmutableSet<Content> content = ImmutableSet.copyOf(resolver.updatedSince(RECENT_TIMESTAMP));
         
         assertEquals(ImmutableSet.of(UPDATED_CONTENT), content);
+    }
+    
+    private static Content createItemWithMediaType(MediaType audio, String uri) {
+        Item item = new Item();
+        item.setCanonicalUri(uri);
+        item.setMediaType(audio);
+        return item;
     }
 }
