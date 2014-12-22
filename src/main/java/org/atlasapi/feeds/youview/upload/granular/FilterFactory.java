cd816566fb68749d11f834805f3ec31e7e9cae7d
Tom McAdam <tom@metabroadcast.com>
2014-12-22 14:05:51 +0000

    Filter items without pcrids

--- src/main/java/org/atlasapi/feeds/youview/upload/granular/FilterFactory.java
+++ src/main/java/org/atlasapi/feeds/youview/upload/granular/FilterFactory.java
@@ -3,16 +3,21 @@ package org.atlasapi.feeds.youview.upload.granular;
 import org.atlasapi.feeds.youview.hierarchy.ItemAndVersion;
 import org.atlasapi.feeds.youview.hierarchy.ItemBroadcastHierarchy;
 import org.atlasapi.feeds.youview.hierarchy.ItemOnDemandHierarchy;
+import org.atlasapi.media.entity.Alias;
+import org.atlasapi.media.entity.Broadcast;
 import org.atlasapi.media.entity.Identified;
 import org.joda.time.DateTime;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
 
 
 public class FilterFactory {
 
+    private static final String TERRESTRIAL_PROGRAMME_CRID_NS = "bbc:terrestrial_programme_crid:teleview";
+    
     private FilterFactory() {
     }
     
@@ -36,13 +41,27 @@ public class FilterFactory {
         return new Predicate<ItemBroadcastHierarchy>() {
             @Override
             public boolean apply(ItemBroadcastHierarchy input) {
-                return hasBeenUpdated(input.item(), updatedSince.get())
-                        || hasBeenUpdated(input.version(), updatedSince.get())
-                        || hasBeenUpdated(input.broadcast(), updatedSince.get());
+                // programme crids aren't present on all broadcasts, and the only reason to
+                // send a broadcast is for the linking between EPG items and on-demands, via
+                // programme crids. Therefore we filter broadcasts without pcrids
+                return hasPCrid(input.broadcast())
+                         && (hasBeenUpdated(input.item(), updatedSince.get())
+                            || hasBeenUpdated(input.version(), updatedSince.get())
+                            || hasBeenUpdated(input.broadcast(), updatedSince.get()));
             }
+
         };
     }
 
+    private static boolean hasPCrid(Broadcast broadcast) {
+        return Iterables.tryFind(broadcast.getAliases(), new Predicate<Alias>() {
+            @Override
+            public boolean apply(Alias input) {
+                return TERRESTRIAL_PROGRAMME_CRID_NS.equals(input.getNamespace());
+            }
+        }).isPresent();
+    };
+    
     public static Predicate<ItemOnDemandHierarchy> onDemandFilter(final Optional<DateTime> updatedSince) {
         if (!updatedSince.isPresent()) {
             return Predicates.alwaysTrue();
