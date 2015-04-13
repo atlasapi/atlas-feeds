931737829e1a92c74a97ac787cd6200ea78469d8
Tom McAdam <tom@metabroadcast.com>
2015-04-13 17:16:49 +0100

    Use new subtitled flag on Location for subtitled

--- src/main/java/org/atlasapi/feeds/youview/nitro/NitroOnDemandLocationGenerator.java
+++ src/main/java/org/atlasapi/feeds/youview/nitro/NitroOnDemandLocationGenerator.java
@@ -99,7 +99,10 @@ public class NitroOnDemandLocationGenerator implements GranularOnDemandLocationG
         }
         instanceDescription.setAVAttributes(generateAvAttributes(encoding));
         instanceDescription.getOtherIdentifier().add(createIdentifierFromPipsIdentifier(content));
-        instanceDescription.getCaptionLanguage().add(captionLanguage(language));
+        
+        if (Boolean.TRUE.equals(encoding.getSubtitled())) {
+            instanceDescription.getCaptionLanguage().add(captionLanguage(language));
+        }
 
         if (Boolean.TRUE.equals(encoding.getSigned())) {
             SignLanguageType signLanguageType = new SignLanguageType();
