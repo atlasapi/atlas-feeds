a27fd0ca545e01ce4522fc4246a701785c20657d
Tom McAdam <tom@metabroadcast.com>
2015-05-07 15:32:39 +0100

    Reset YouView epoch

--- src/main/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolver.java
+++ src/main/java/org/atlasapi/feeds/youview/resolution/UpdatedContentResolver.java
@@ -1,7 +1,7 @@
 package org.atlasapi.feeds.youview.resolution;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static org.joda.time.DateTimeConstants.JANUARY;
+import static org.joda.time.DateTimeConstants.APRIL;
 
 import java.util.Iterator;
 
@@ -22,7 +22,7 @@ public class UpdatedContentResolver implements YouViewContentResolver {
 
     @VisibleForTesting
     // TODO update this to point back to the genuine start of time once testing is complete
-    static final DateTime START_OF_TIME = new DateTime(2015, JANUARY, 20, 0, 0, 0, 0, DateTimeZones.UTC);
+    static final DateTime START_OF_TIME = new DateTime(2015, APRIL, 26, 0, 0, 0, 0, DateTimeZones.UTC);
 
     private static final Predicate<Content> IS_VIDEO_CONTENT = new Predicate<Content>() {
         @Override
