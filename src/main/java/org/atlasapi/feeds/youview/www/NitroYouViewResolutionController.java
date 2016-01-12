d4c98f8202da5ea73dc7c3b4063777e9da291471
Jamie <jamie@metabroadcast.com>
2016-01-12 16:42:12 +0000

    CheckArguments

--- src/main/java/org/atlasapi/feeds/youview/www/NitroYouViewResolutionController.java
+++ src/main/java/org/atlasapi/feeds/youview/www/NitroYouViewResolutionController.java
@@ -1,5 +1,6 @@
 package org.atlasapi.feeds.youview.www;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.IOException;
@@ -74,12 +75,8 @@ public class NitroYouViewResolutionController {
     }
 
     private ResolutionApiOutput outputFor(String pid, String crid) {
-        if (pid == null && crid == null) {
-            throw new IllegalArgumentException("Must specify pid or crid");
-        }
-        if (pid != null && crid != null) {
-            throw new IllegalArgumentException("Must not specify both pid and crid");
-        }
+        checkArgument(pid == null && crid == null, "Must specify pid or crid");
+        checkArgument(pid != null && crid != null, "Must not specify both pid and crid");
         
         if (pid != null) {
             return outputFor(VERSION_PID_TYPE, pid, YOUVIEW_CRID_TYPE, mappingStore.getValueFor(canonicalUriFor(pid)));
