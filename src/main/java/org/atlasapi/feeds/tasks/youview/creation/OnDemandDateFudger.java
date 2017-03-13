1a8704f5e9a5f93b17124ab0d75afc42ae5276c9
Emils Solmanis <emils@metabroadcast.com>
2017-03-13 17:03:45 +0000

    Fudge OD dates back to past if they're expired
    This is a hack around The Beeb deleting availabilities, then reinstating them, and that process
    interacting with our hash logic. Fudging the dates before generating the payloads makes sure we
    bust the hashes and resend an OD to YV.

---
+++ src/main/java/org/atlasapi/feeds/tasks/youview/creation/OnDemandDateFudger.java
@@ -0,0 +1,58 @@
+package org.atlasapi.feeds.tasks.youview.creation;
+
+import org.atlasapi.feeds.youview.hierarchy.ItemOnDemandHierarchy;
+import org.atlasapi.media.entity.Location;
+import org.atlasapi.media.entity.Policy;
+
+import com.metabroadcast.common.time.Clock;
+
+import org.joda.time.DateTime;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/** This exists because
+ * 1. The BBC "sometimes" completely nuke availabilites
+ * 2. We decided to handle that case in R4 and send DELETEs for those to YV
+ * 3. In R3 we decided to decrease the amount of "entity unchanged" transactions generated
+ *    via payload hashes
+ * 4. This then means that BBC flap an availability, we send both the payload and the matching
+ *    DELETE, and then nothing once BBC republish it, because we have both hashes now
+ *
+ * This class is used in the JAXB generator to artificially fudge the start dates of Locations if
+ * the start date is in the past, thus changing the payload, busthing the hash and resulting in the
+ * OnDemand being uploaded. What we do is hard.
+ */
+public class OnDemandDateFudger {
+
+    private final Clock clock;
+
+    private OnDemandDateFudger(Clock clock) {
+        this.clock = checkNotNull(clock);
+    }
+
+    public static OnDemandDateFudger create(Clock clock) {
+        return new OnDemandDateFudger(clock);
+    }
+
+    public ItemOnDemandHierarchy fudgeStartDates(ItemOnDemandHierarchy onDemandHierarchy) {
+        Location resultLocation = onDemandHierarchy.location();
+
+        DateTime now = clock.now();
+
+        Policy policy = resultLocation.getPolicy();
+        if (policy != null && policy.getAvailabilityStart().isBefore(now)) {
+            Policy resultPolicy = policy.copy();
+            resultPolicy.setAvailabilityStart(now.minusHours(6));
+
+            resultLocation = resultLocation.copy();
+            resultLocation.setPolicy(resultPolicy);
+        }
+
+        return new ItemOnDemandHierarchy(
+                onDemandHierarchy.item(),
+                onDemandHierarchy.version(),
+                onDemandHierarchy.encoding(),
+                resultLocation
+        );
+    }
+}
