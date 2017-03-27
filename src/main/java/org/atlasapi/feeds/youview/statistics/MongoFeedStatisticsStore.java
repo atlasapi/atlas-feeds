12c84c23203e1b301b480677a27e721df7c57f06
Dragos Pitica <piticadragos@yahoo.com>
2017-03-27 16:04:42 +0100

    fixed mongo queries

--- src/main/java/org/atlasapi/feeds/youview/statistics/MongoFeedStatisticsStore.java
+++ src/main/java/org/atlasapi/feeds/youview/statistics/MongoFeedStatisticsStore.java
@@ -88,11 +88,12 @@ public class MongoFeedStatisticsStore implements FeedStatisticsResolver {
     private int getTasksCreatedInTheLastFourHours() {
         Date fourHoursAgo = DateTime.now().minusHours(4).toDate();
 
-        DBObject publishedClause = QueryBuilder.start("status").is(Status.PUBLISHED).get();
-        DBObject acceptedClause = QueryBuilder.start("status").is(Status.ACCEPTED).get();
+        DBObject publishedClause = QueryBuilder.start("status").is("PUBLISHED").get();
+        DBObject acceptedClause = QueryBuilder.start("status").is("ACCEPTED").get();
 
-        DBObject query = QueryBuilder.start("created").greaterThanEquals(fourHoursAgo)
+        DBObject query = QueryBuilder.start()
                 .or(publishedClause, acceptedClause)
+                .and("created").greaterThanEquals(fourHoursAgo)
                 .get();
 
         return collection.find(query).count();
@@ -101,8 +102,9 @@ public class MongoFeedStatisticsStore implements FeedStatisticsResolver {
     private int getTasksFailedInTheLastFourHours() {
         Date fourHoursAgo = DateTime.now().minusHours(4).toDate();
 
-        DBObject query = QueryBuilder.start("created").greaterThanEquals(fourHoursAgo)
-                .and("status").is(Status.REJECTED)
+        DBObject query = QueryBuilder.start()
+                .put("created").greaterThanEquals(fourHoursAgo)
+                .and("status").is("REJECTED")
                 .get();
 
         return collection.find(query).count();
