9b8334a22459881b79c5c4196befbde69654af0f
andreas <andreas@metabroadcast.com>
2017-08-04 13:32:08 +0100

    going back to telescopev0.8 and INGEST events to see if that is causing the issue with atlas

--- src/main/java/org/atlasapi/reporting/telescope/FeedsTelescopeProxy.java
+++ src/main/java/org/atlasapi/reporting/telescope/FeedsTelescopeProxy.java
@@ -53,7 +53,7 @@ public class FeedsTelescopeProxy extends TelescopeProxy {
         //if all went well
         Event reportEvent = Event.builder()
                 .withStatus(Event.Status.SUCCESS)
-                .withType(Event.Type.UPLOAD)
+                .withType(Event.Type.INGEST)
                 .withEntityState(EntityState.builder()
                         .withAtlasId(atlasItemId)
                         .withRaw(payload)
@@ -91,7 +91,7 @@ public class FeedsTelescopeProxy extends TelescopeProxy {
 
         Event reportEvent = Event.builder()
                 .withStatus(Event.Status.FAILURE)
-                .withType(Event.Type.UPLOAD)
+                .withType(Event.Type.INGEST)
                 .withEntityState(
                         EntityState.builder()
                         .withAtlasId(atlasItemId)
@@ -122,7 +122,7 @@ public class FeedsTelescopeProxy extends TelescopeProxy {
 
         Event reportEvent = Event.builder()
                 .withStatus(Event.Status.FAILURE)
-                .withType(Event.Type.UPLOAD)
+                .withType(Event.Type.INGEST)
                 .withEntityState(EntityState.builder()
                         .withError(errorMsg)
                         .withRaw(payload)
