3136d046dc3575c7a53e56be6ee184801d2eec49
@alianos- <alianos.home@gmail.com>
2018-01-22 17:22:28 +0000

    bug fix

--- src/main/java/tva/metadata/custom/ProgramInformationType4300CP.java
+++ src/main/java/tva/metadata/custom/ProgramInformationType4300CP.java
@@ -5,6 +5,7 @@ package tva.metadata.custom;
 //to modify them. The 4300CP is in reference to the YV specification document that requires the
 //changes done here.
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -14,6 +15,7 @@ import javax.xml.bind.annotation.XmlType;
 
 import com.youview.refdata.schemas._2011_07_06.ExtendedTargetingInformationType;
 import tva.metadata._2010.ProgramInformationType;
+import tva.mpeg7._2008.UniqueIDType;
 
 /**
  * This class adds a targeting information field to the ProgramInformation element.
@@ -31,11 +33,9 @@ public class ProgramInformationType4300CP extends ProgramInformationType {
     protected List<ExtendedTargetingInformationType> TargetingInformation;
 
     public List<ExtendedTargetingInformationType> getTargetingInformation() {
+        if (TargetingInformation == null) {
+            TargetingInformation = new ArrayList<ExtendedTargetingInformationType>();
+        }
         return TargetingInformation;
-    }
-
-    public void setTargetingInformation(
-            List<ExtendedTargetingInformationType> targetingInformation) {
-        TargetingInformation = targetingInformation;
     }
 }
