f28ad67c99de4860029a7191cc56d582587333a4
@alianos- <alianos.home@gmail.com>
2018-04-27 16:31:41 +0100

    original

--- src/main/java/org/atlasapi/feeds/tasks/youview/creation/DeltaTaskCreationTask.java
+++ src/main/java/org/atlasapi/feeds/tasks/youview/creation/DeltaTaskCreationTask.java
@@ -136,28 +136,6 @@ public class DeltaTaskCreationTask extends TaskCreationTask {
                 continue;
             }
 
-            Content mergedContent;
-            try {
-                mergedContent = youviewContentMerger.equivAndMerge(updatedContent);
-            } catch (Exception e) {
-                log.error("Failed during the attempt to equiv, merge or get a repId. "
-                          + "This item will not be pushed to YV. Content {}. ",
-                        updatedContent.getCanonicalUri(), e);
-                continue;
-            }
-
-            try {
-                AmazonContentConsolidator.consolidate(mergedContent); //mutates the item
-            } catch (Exception e) {
-                log.error("Failed during the attempt to consolidate versions. "
-                          + "This item will not be pushed to YV. Content {}. ",
-                        updatedContent.getCanonicalUri(), e);
-                continue;
-            }
-
-            uploadProcessor.process(mergedContent);
-            reportStatus("Uploads: " + uploadProcessor.getResult());
-
         }
         return deleted;
     }
